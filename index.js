b3b71c9a1c545a47a7fa7c56f492c5316c81526b
asandamhlongo <mhlongoasanda2002@gmail.com>
2024-02-22 15:14:47 +0200

    Added axios and injected api in my javascript code

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+    let apiKey = "125bf6o454dd320dtf3a85f9830bacf6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#current-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 
 }
+
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);

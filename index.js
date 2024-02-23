331fb6864116d25ac41d9ecb27cf41fafb9a6d25
asandamhlongo <mhlongoasanda2002@gmail.com>
2024-02-23 17:24:06 +0200

    Added a function for real time and injected more weather data

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
-  
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000)
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formateDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity + " "+ "%";
+  windSpeedElement.innerHTML = response.data.wind.speed + " "+ "km/h";
   temperatureElement.innerHTML = Math.round(temperature);
 
+}
+function formateDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  return day + " "+hours + ":"+ (date.getMinutes() < 10 ? '0': '') + minutes + "," + " ";
 }
 
 function searchCity(city) {
